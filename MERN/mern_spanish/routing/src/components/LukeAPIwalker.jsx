98fb962e89bfdfa6814c110dc2d2b7a0228c051c
Cubilla <cubilladev@gmail.com>
2024-01-14 08:49:08 -0300

    Peticiones api

--- MERN/mern_spanish/routing/src/components/LukeAPIwalker.jsx
+++ MERN/mern_spanish/routing/src/components/LukeAPIwalker.jsx
@@ -1,28 +1,82 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-const LukeAPIwalker = () => {
-    const [ data, setData ] = useState([]);
+import { useState, useEffect } from 'react';import axios from 'axios';
+import{ useParams }from 'react-router-dom'
 
-    useEffect(()=>{
-        axios.get('')
-            .then()
-    }, [data])
+const LukeAPIwalker = () => {
+    const [ id, setId ] = useState('');
+    const [ data, setData ] = useState({});    
+    const [ dataPar, setDataPar ] = useState('people');
+    const [error, setError] = useState(null);
+    const {paramId } = useParams()
 
     const handleChange = (e) =>{
-        setData(e.target.value);
+        setDataPar(e.target.value);
     }
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                if (paramId !== undefined) {
+                    await axios.get(`https://swapi.dev/api/${dataPar}/${paramId}/`)
+                    .then(response => {
+                        setData(response.data);
+                        console.log('Petición exitosa:', response.data);
+                        setError(null)
+                    });
+                } else {
+                    console.log('No se cumple la condición para hacer la petición.');
+                }
+            } catch (error) {
+                console.error('Error fetching data:', error);
+                setError(error)
+            }
+        };
+    
+        fetchData();
+    }, []);
+
+    const search = async (e) => {
+        try {
+            e.preventDefault();
+            const response = await axios.get(`https://swapi.dev/api/${dataPar}/${id}/`);
+            setData(response.data);
+            console.log(response.data)
+            setError(null)
+        } catch (e) {
+            console.error('Hubo error en la peticion ', e);
+            setError(e)
+        }
+    }
+
+
     return (
         <div>
-            <div>
-                <p>Search for:</p>
-                <select value={data} onChange={handleChange}>
-                    {
-                        data.map((items, index)=>{
-                            <option key={index}>{items}</option>
-                        })
-                    }
-                </select>
-            </div>
+            <form onSubmit={search}>
+                <div>
+                    <p>Search for:</p>
+                    <select value={dataPar} onChange={handleChange}>
+                    <option value={'people'}>People</option>
+                    <option value={'films'}>films</option>
+                    <option value={'starships'}>starships</option>
+                    <option value={'vehicles'}>vehicles</option>
+                    <option value={'species'}>species</option>
+                    <option value={'planets'}>Planets</option>
+                    </select>
+                    <label>id:</label>
+                    <input value={id} onChange={(e) => setId(e.target.value)} />
+                    <button type="submit">Search</button>
+                </div>
+            </form>
+            {
+                error && 
+                <div>
+                    <h3>Estos no son los droides que está buscando</h3>
+                    <img src='../public/img/Obi-Wan_Kenobi.jpg'/>
+                </div>
+            }
+            <h3>{data.name}</h3>
+            <h3>{data.height}</h3>
+            <h3>{data.mass}</h3>
+            <h3>{data.hair_color}</h3>
+            <p></p>
         </div>
     )
 }
